27c3c74d4db456942fa3a177d9a8ac8bc13f31cb
Lokesh Vadlamudi <lokesh.dev96@gmail.com>
2019-11-02 14:35:39 -0700

    Update RiceBowl.py

--- RiceBowl.py
+++ RiceBowl.py
@@ -1,35 +1,23 @@
 import inquirer
 class RiceBowl:
-    def addRiceType(self,ricetype):
-        self.riceType = ricetype
-
-    def addMixVeg(self,mixveg):
-        self.mixVeg = mixveg
-
-    def addMeatType(self,meattype):
-        self.meatType = meattype
-
-    def addSauceType(self,saucetype):
-        self.sauceType = saucetype
-
-    def addSourCream(self,sourcream):
-        self.sourCream = sourcream
-
-    def addGuacamole(self,guacamole):
-        self.Guacamole = guacamole
-
-#creating the object
+    def addIngredients(self, ings):
+        self.ingredients = ings
+    def show(self):
+        print(self.ingredients)
 rb = RiceBowl()
-
 a = [["white", "brown"],["add","skip"],["chicken","beef"],["spicy","sweet"],["add","skip"],["add","skip"]]
-b = ["rice?","mix veg?","meat?","sauce?","sour cream?","guacamole?"]
-c = [rb.addRiceType,rb.addMixVeg,rb.addMeatType,rb.addSauceType,rb.addSourCream,rb.addGuacamole]
-
-for option,question,fun in zip(a,b,c):
+b = ["rice","mix veg","meat","sauce","sour cream","guacamole"]
+d = {}
+for option,question in zip(a,b):
     choices = [inquirer.List('chose',choices = option,message = question)]
     answer = inquirer.prompt(choices)
-    fun(answer["chose"])
+    d[question] = (answer["chose"])
+rb.addIngredients(d)
+rb.show()
 
-#printing the ricebowl ingredients :
-from pprint import pprint
-pprint(vars(rb))
+
+
+
+
+
+
